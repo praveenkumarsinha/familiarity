b55a54d042a5504874f8ffc2fd788e0ef94863cd
Praveen Kumar Sinha <praveen@headerlabs.com>
2014-11-07 00:42:00 +0530

    Adding familiarity view on page change (and also storing it iin cookie)

--- app/assets/javascripts/familiarity/application.js
+++ app/assets/javascripts/familiarity/application.js
@@ -10,5 +10,26 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require ../familiarity/jquery.cookie
 //= require ../familiarity/jquery.tooltipster
 //= require_tree .
+
+// Show and hide on F1 and ESC key respectively
+$(document).keyup(function (e) {
+    if (e.keyCode == 27 && familiarityView_rendered) { //ESC key pressed
+        familiarityView(false);
+    }
+    if (e.keyCode == 112 && !familiarityView_rendered) { //F1 key pressed
+        familiarityView();
+    }
+});
+
+$(document).on("page:change", function () {
+    if (typeof $.cookie('familiarity') == "undefined") {
+        familiarityView();
+        console.log($.cookie('familiarity', new Date(), {
+            path: window.location.pathname,
+            expires: 0.001 //TODO: This should be no of days and should be set to some valid tangible figure
+        }));
+    }
+});
