2cbe867867ff887b719ab64aa00853806d570437
Praveen Kumar Sinha <praveen@headerlabs.com>
2014-11-08 01:35:18 +0530

    Adding Familiarity new prototyped JS for view

--- app/assets/javascripts/familiarity/application.js
+++ app/assets/javascripts/familiarity/application.js
@@ -14,19 +14,11 @@
 //= require ../familiarity/jquery.tooltipster
 //= require_tree .
 
-// Show and hide on F1 and ESC key respectively
-$(document).keyup(function (e) {
-    if (e.keyCode == 27 && familiarityView_rendered) { //ESC key pressed
-        familiarityView(false);
-    }
-    if (e.keyCode == 112 && !familiarityView_rendered) { //F1 key pressed
-        familiarityView();
-    }
-});
 
 $(document).on("page:change", function () {
+    var familiarity = new Familiarity();
     if (typeof $.cookie('familiarity') == "undefined") {
-        familiarityView();
+        familiarity.familiarityView(true);
         console.log($.cookie('familiarity', new Date(), {
             path: window.location.pathname,
             expires: 0.001 //TODO: This should be no of days and should be set to some valid tangible figure
